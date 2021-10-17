50f7509d05087717260ba8624f9aefe2f69fc7f5
Pfizz <pfiste80@gmail.com>
2021-10-17 01:08:31 -0400

    end of 4.2.6

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,22 @@
-var tasksToDoEl = document.querySelector("#tasks-to-do");
-const buttonEl = document.querySelector("#save-task");
+const formEl = document.querySelector("#task-form");
+const tasksToDoEl = document.querySelector("#tasks-to-do");
 
 
-const createTaskHandler = () => {
+
+const createTaskHandler = (event) => {
+    event.preventDefault();
+    const taskNameInput = document.querySelector("input[name='task-name']").value;
+    const taskTypeInput = document.querySelector("select[name='task-type']").value;
     const listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task";
+    const taskInfoEl = document.createElement("div");
+    taskInfoEl.className = "task-info";
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+
+    listItemEl.appendChild(taskInfoEl);
     tasksToDoEl.appendChild(listItemEl);
 };
 
-buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
 
 
